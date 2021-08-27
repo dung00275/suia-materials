6f94b4c40f7a679fdb58799cb73161048321e342
dung.vu <dungvh@galaxy.com.vn>
2021-08-27 17:47:11 +0700

    [ App ] Update

--- 02-planning-a-paged-app/projects/starter/HIITFit/HIITFit/Views/ContentView.swift
+++ 02-planning-a-paged-app/projects/starter/HIITFit/HIITFit/Views/ContentView.swift
@@ -34,8 +34,13 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    TabView {
+        WelcomeView()
+        ForEach(0..<4) { index in
+            ExerciseView(index: index)
+        }
+        Text("Excercise 2")
+    }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
   }
 }
 
